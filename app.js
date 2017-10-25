4a03d5b4072b57fb69f3c92040400fe76d1d0190
Charles Hoshman <t.hoshman@gmail.com>
2017-10-25 10:41:25 -0700

    added grocery list"

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+// var GroceryList =()=>(<ul><li>Red Bul</li><li>Ben and Jerrys</li></ul>);
+
+// ReactDOM.render(<GroceryList />, document.getElementById("app"))
+
+var GroceryList = function GroceryList() {
+  return React.createElement(
+    "ul",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Red Bul"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Ben and Jerrys"
+    )
+  );
+};
+
+ReactDOM.render(React.createElement(GroceryList, null), document.getElementById("app"));
