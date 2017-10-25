5c362629adbbab77f68f26e72c929c385f1d8fdd
dorktobes <t.hoshman@gmail.com>
2017-10-25 12:20:12 -0700

    completed the react components course

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-// var GroceryList =()=>(<ul><li>Red Bul</li><li>Ben and Jerrys</li></ul>);
-
-// ReactDOM.render(<GroceryList />, document.getElementById("app"))
-
-var GroceryList = function GroceryList() {
-  return React.createElement(
-    "ul",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "Red Bul"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Ben and Jerrys"
-    )
-  );
+var GroceryList =(props)=>{
+  return (<ul> 
+    {
+      props.items.map(item => 
+        <GroceryListItem item={item} />  
+      )
+    }
+  </ul>)
+};
+  
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
+  onGroceryItemHover() {
+    this.setState({hover: !this.state.hover});
+  }
+  render(){
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'none'
+    }
+    return <li style={style} onMouseOver={this.onGroceryItemHover.bind(this)} onMouseExit={this.onGroceryItemHover.bind(this)}>{this.props.item}</li>;
+  }
 };
 
-ReactDOM.render(React.createElement(GroceryList, null), document.getElementById("app"));
+// var GroceryListItem = (prop) => {
+//    var clickedItem = (event) => {
+//     console.log('I got clicked');
+//   };
+// return <li onClick={clickedItem}>{prop.item}</li>;
+// };
+var items = ['Manchego', 'Red Bull', 'Ben and Jerrys', 'Wine', 'Beer', 'Candy'];
+ReactDOM.render(<GroceryList items={items} />, document.getElementById("app"))
+
+
+
